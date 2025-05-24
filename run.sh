cc6c85421779f9fe96141ccf51c110c1d13ec3f1
sachin <sachinthapa@gmail.com>
2025-05-24 16:12:49 +0545

    script mod

--- run.sh
+++ run.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
-GITHUB_USERNAME=$1
-GITHUB_EMAIL=$2
+SERVICE_NAME=$1
+RELEASE_VERSION=$2
 
-SERVICE_NAME=order
-RELEASE_VERSION=v1.2.3
-
-echo "Generating Go source code"
-mkdir -p golang
-protoc --go_out=./golang \
-  --go_opt=paths=source_relative \
-  --go-grpc_out=./golang \
-  --go-grpc_opt=paths=source_relative \
- ./${SERVICE_NAME}/*.proto
-
-echo "Generated Go source code files"
-ls -al ./golang/${SERVICE_NAME}
-
+sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+protoc --go_out=./golang --go_opt=paths=source_relative \
+--go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
+./${SERVICE_NAME}/*.proto
 cd golang/${SERVICE_NAME}
 go mod init \
-  github.com/${GITHUB_USERNAME}/grpc-microservices-in-go/ch03/listing_3.2/golang/${SERVICE_NAME} || true
-go mod tidy || true
+github.com/huseyinbabal/microservices-proto/golang/${SERVICE_NAME} ||true
+go mod tidy
 cd ../../
-git config --global user.email ${GITHUB_EMAIL}
-git config --global user.name ${GITHUB_USERNAME}
+git config --global user.email "mailsachinthapa@gmail.com"
+git config --global user.name "sachin"
 git add . && git commit -am "proto update" || true
-git push -u origin HEAD
-git tag -d ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}
-git push --delete origin ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}
-git tag -fa ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION} \
-  -m "ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}"
-git push origin refs/tags/ch03/listing_3.2/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
+-m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
